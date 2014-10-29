678fc0f06e45e876774504cb15e7b510eea8c1f1
Sarat AR <sarath@awsm.in>
2014-10-29 12:38:20 +0530

    Embed doc popup layout changes

--- inc/popup.php
+++ inc/popup.php
@@ -4,7 +4,7 @@
         <div id="embed_message">
         </div>
 		<div id="popup-header">
-		<h1><?php _e('Embed Any Document by AWSM.in','ead');?></h1>
+		<h1><?php _e('Add Document','ead');?></h1>
 		</div>
 		<div class="section">
         <ul class="tabs">
@@ -13,27 +13,31 @@
         </ul>
         <form action="" onSubmit="return false" method="post" enctype="multipart/form-data" id="Docuploader">
         <div class="box visible">
-        	<strong><?php _e('Upload Document','ead');?></strong><br/>
-             <input type="button"  value="Add Document" class="button-primary" id="upload_doc"/> 
+             <div class="text-center"><input type="button"  value="Upload Document" class="ead-btn button-primary button-large" id="upload_doc"/></div> 
+             <div class="upload-success">
+                <div class="uploaded-doccument">
+                    <span>Doccument.pdf</span>
+                    230.6kb
+                </div>
+                <a class="ead-btn button" id="adv_options"><?php _e('Show Advanced Options','ead');?></a>
+                <div class="clear"></div>
+             </div>
         </div>
         <div class="box">
-         <strong><?php _e('Add From URL','ead');?></strong><br/>
-            <input name="awsm_url" type="text" class="opt dwl" id="awsm_url" style="width:200px;"/><br/>
-            <input type="button"  value="Add Url" class="button-primary" id="add_url"/>  
+         <label for="awsm_url"><?php _e('Enter document URL','ead');?></label>
+            <input name="awsm_url" type="text" class="opt dwl input-group-text" id="awsm_url"/>
+            <input type="button"  value="Add Url" class="ead-btn button-primary input-group-btn" id="add_url"/>  
         </div>
         </div>
-        <table width="100%" border="0" cellspacing="0" cellpadding="5">
-        <tr>
+        <div class="advanced_options">
+        <table>
+            <tr>
             <td colspan="2">
                 <br />
                 <strong><?php _e('Shortcode Preview', 'ead'); ?></strong><br/>
                 <textarea name="shortcode" style="width:100%" id="shortcode" readonly="readonly"></textarea>
             </td>
         </tr> 
-        </table>
-        <a class="button" id="adv_options"><?php _e('Show Advanced Options','ead');?></a>
-        <div class="advanced_options">
-        <table>
             <tr>
                 <td><label>Width</label> <input name="width"  class="embedval" id="ead_width" value="<?php echo get_option('ead_width', '100%' );?>"></td>
                 <td><label>Height</label> <input name="height" class="embedval" id="ead_height"  value="<?php echo get_option('ead_height', '100%' );?>"></td>
@@ -50,12 +54,13 @@
         </div>
         <div class="mceActionPanel">
         <div style="float: left">
-            <input type="button" id="insert_doc" name="insert" class="button button-primary button-small" value="<?php _e('Insert', 'ead'); ?>" />
+            <input type="button" id="insert_doc" name="insert" class="ead-btn button button-primary button-medium" value="<?php _e('Insert', 'ead'); ?>" />
         </div>
         
         <div style="float: right">
-            <input type="button" class="cancel_embed" name="cancel"  class="button button-small" value="<?php _e('Cancel', 'ead'); ?>" />
+            <input type="button" name="cancel"  class="ead-btn button cancel_embed button-medium" value="<?php _e('Cancel', 'ead'); ?>" />
         </div>
+        <div class="clear"></div>
         </div>
 	</div>	
 </div>
