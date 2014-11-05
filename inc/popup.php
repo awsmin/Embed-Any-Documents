de10af842eb73993e8fe202246c746d1e190a52d
Adhun Anand <adhun@awsm.in>
2014-11-05 11:19:58 +0530

    New Structur popup

--- inc/popup.php
+++ inc/popup.php
@@ -1,58 +1,59 @@
 <?php if ( ! defined( 'ABSPATH' ) ) { exit; } ?> 
 <div id="embed-popup-wrap">
-	<div id="embed-popup">
-        <div id="embed_message">
+    <div id="embed-popup">
+        <button title="Close (Esc)" type="button" class="mfp-close">×</button>
+        <div id="popup-header">
+        <h1><?php _e('Add Document','ead');?></h1>
         </div>
-		<div id="popup-header">
-		<h1><?php _e('Add Document','ead');?></h1>
-		</div>
-		<div class="section">
-        <ul class="tabs">
-            <li class="current"><?php _e('Upload and Embed','ead');?></li>
-            <li><?php _e('Add From URL','ead');?></li>
-        </ul>
-        <form action="" onSubmit="return false" method="post" enctype="multipart/form-data" id="Docuploader">
-        <div class="box visible">
-             <div class="text-center"><input type="button"  value="Upload Document" class="ead-btn button-primary button-large" id="upload_doc"/></div> 
-             <div class="upload-success">
+        <div class="section">
+            <div id="embed_message" class="awsm-error" style="display:none;"><p></p></div>
+            <div class="ead_container">
+            <ul class="tabs">
+                <li class="current"><?php _e('Upload and Embed','ead');?></li>
+                <li><?php _e('Add From URL','ead');?></li>
+            </ul>
+            <form action="" onSubmit="return false" method="post" enctype="multipart/form-data" id="Docuploader">
+            <div class="box visible">
+                 <div class="text-center"><input type="button"  value="Upload Document" class="ead-btn button-primary button-large" id="upload_doc"/></div> 
+            
+            </div>
+            <div class="box">
+             <label for="awsm_url"><?php _e('Enter document URL','ead');?></label>
+                <input name="awsm_url" type="text" class="opt dwl input-group-text" id="awsm_url"/>
+                <input type="button"  value="Add URL" class="ead-btn button-primary input-group-btn" id="add_url"/>  
+            </div>            
+            </form>
+            </div><!--ead_container-->
+            <div class="upload-success">
                 <div class="uploaded-doccument">
-                    <span>Doccument.pdf</span>
-                    230.6kb
+                    <p id="ead_filename"></p>
+                    <span id="ead_filesize"></span>
                 </div>
                 <a class="ead-btn button" id="adv_options"><?php _e('Show Advanced Options','ead');?></a>
                 <div class="clear"></div>
-             </div>
-        </div>
-        <div class="box">
-         <label for="awsm_url"><?php _e('Enter document URL','ead');?></label>
-            <input name="awsm_url" type="text" class="opt dwl input-group-text" id="awsm_url"/>
-            <input type="button"  value="Add Url" class="ead-btn button-primary input-group-btn" id="add_url"/>  
-        </div>
-        </div>
-        <div class="advanced_options">
-        <table>
-            <tr>
-            <td colspan="2">
-                <br />
-                <strong><?php _e('Shortcode Preview', 'ead'); ?></strong><br/>
-                <textarea name="shortcode" style="width:100%" id="shortcode" readonly="readonly"></textarea>
-            </td>
-        </tr> 
-            <tr>
-                <td><label>Width</label> <input name="width"  class="embedval" id="ead_width" value="<?php echo get_option('ead_width', '100%' );?>"></td>
-                <td><label>Height</label> <input name="height" class="embedval" id="ead_height"  value="<?php echo get_option('ead_height', '100%' );?>"></td>
-            </tr>
-            <tr>
-            <td><label><?php _e('Show Download Link','ead');?></label> 
-            <?php 
-            $downoptions= array('alluser' => __('For all users',$this->text_domain),'logged' => __('For Logged-in users',$this->text_domain),'none' => __('None',$this->text_domain));
-            ead_selectbuilder('ead_download', $downoptions,esc_attr( get_option('ead_download')),'embed_download'); 
-            ?> 
-            </td>
-            </tr>
-        </table>
-        </div>
-        <div class="mceActionPanel">
+            </div>
+            <div class="advanced_options">
+            <ul class="option-fields">
+              
+                <li>
+                    <div class="f-left"><label>Width</label> <input type="text" name="width"  class="embedval input-small" id="ead_width" value="<?php echo get_option('ead_width', '100%' );?>"></div>
+                    <div class="f-left"><label>Height</label> <input type="text" name="height" class="embedval input-small" id="ead_height"  value="<?php echo get_option('ead_height', '100%' );?>"></div>
+                    <div class="clear"></div>
+                </li>
+                <li>
+               <label><?php _e('Show Download Link','ead');?></label> 
+                <?php 
+                $downoptions= array('alluser' => __('For all users',$this->text_domain),'logged' => __('For Logged-in users',$this->text_domain),'none' => __('None',$this->text_domain));
+                ead_selectbuilder('ead_download', $downoptions,esc_attr( get_option('ead_download')),'embed_download'); 
+                ?> 
+                </li>
+                 <li>
+                    <label><?php _e('Shortcode Preview', 'ead'); ?></label>
+                    <textarea name="shortcode" style="width:100%" id="shortcode" readonly="readonly"></textarea>
+                </li>            
+                </ul>
+            </div>
+        <div class="mceActionPanel" style="display:none;">
         <div style="float: left">
             <input type="button" id="insert_doc" name="insert" class="ead-btn button button-primary button-medium" value="<?php _e('Insert', 'ead'); ?>" />
         </div>
@@ -62,5 +63,6 @@
         </div>
         <div class="clear"></div>
         </div>
-	</div>	
+    </div>  
+    </div>
 </div>
