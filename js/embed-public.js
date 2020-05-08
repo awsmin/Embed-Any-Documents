80a65a077a584cb8fde55a35363d577815738e2d
Anantajit JG <anantajitjg2010@gmail.com>
2020-05-08 20:32:33 +0530

    UPDATE: Remove reattaching 'load' event on iframe when reload button is clicked

--- js/embed-public.js
+++ js/embed-public.js
@@ -1,16 +1,12 @@
 jQuery(function($) {
-    function displayEmbeddedDocument($iframe) {
-        $iframe.on('load', function() {
-            $(this).parents('.ead-document').find('.ead-document-loading').css('display', 'none');
-        });
-    }
-    displayEmbeddedDocument($('.ead-iframe-wrapper .ead-iframe'));
+    $('.ead-iframe-wrapper .ead-iframe').on('load', function() {
+        $(this).parents('.ead-document').find('.ead-document-loading').css('display', 'none');
+    });
 
     $(document).on('click', '.ead-reload-btn', function(e) {
         e.preventDefault();
         var $wrapper = $(this).parents('.ead-document');
         var iframeSrc = $wrapper.find('.ead-iframe').attr('src');
         $wrapper.find('.ead-iframe').attr('src', iframeSrc);
-        displayEmbeddedDocument($wrapper.find('.ead-iframe'));
     });
 });
