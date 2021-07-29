bdcac67029b60c3c1c7c9cca41a2b79a6c914642
sruthihari <35758330+sruthihari@users.noreply.github.com>
2021-07-29 15:43:37 +0530

    icon change

--- blocks/document/modules/icon.js
+++ blocks/document/modules/icon.js
@@ -1,4 +1,24 @@
 const icon = {
-    block: <svg aria-hidden="true" role="img" focusable="false" class="dashicon dashicons-media-document" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"><path d="M12 2l4 4v12H4V2h8zM5 3v1h6V3H5zm7 3h3l-3-3v3zM5 5v1h6V5H5zm10 3V7H5v1h10zM5 9v1h4V9H5zm10 3V9h-5v3h5zM5 11v1h4v-1H5zm10 3v-1H5v1h10zm-3 2v-1H5v1h7z"></path></svg>
+    block: <svg width="30" height="30" viewBox="0 0 93 59" version="1.1">
+    <title>Group 6</title>
+    <g id="Home" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+        <g id="Products" transform="translate(-1418.000000, -559.000000)">
+            <g id="Group-4-Copy" transform="translate(680.000000, 559.286325)">
+                <g id="Group-5" transform="translate(410.000000, 0.000000)">
+                    <g id="Group-6" transform="translate(331.500000, 0.000000)">
+                        <g id="EAD-logo-op-Copy" transform="translate(18.728338, 0.000000)" fill="#000000" fill-rule="nonzero">
+                            <g id="EAD-logo-op">
+                                <path d="M33.5750746,0 C34.5712497,0 35.5242256,0.406355696 36.2133321,1.12497174 L36.2133321,1.12497174 L49.0601427,14.5219184 L49.0601427,54.2222222 C49.0601427,55.2015977 48.6695389,56.1241177 47.9900822,56.8028598 C47.3106256,57.4816019 46.3871344,57.8717949 45.4067278,57.8717949 L45.4067278,57.8717949 L3.65341488,57.8717949 C2.67300815,57.8717949 1.7495169,57.4816018 1.0700602,56.8028595 C0.390603772,56.1241174 0,55.2015976 0,54.2222222 L0,54.2222222 L0,3.64957265 C0,2.6701971 0.39060381,1.74767718 1.07006047,1.06893504 C1.74951711,0.39019302 2.67300824,0 3.65341488,0 L3.65341488,0 Z M30.9042766,5.05039756 L30.8603293,5.0955805 C30.4821979,5.49575597 30.2711519,6.02818175 30.2711519,6.58195582 L30.2711519,16.6189013 C30.2711519,17.8064955 31.2221799,18.7692308 32.3953323,18.7692308 L42.760631,18.7692308 C43.9337836,18.7692308 44.8848114,17.8064954 44.8848114,16.6189013 C44.8848114,16.0320337 44.6478651,15.4706638 44.2289321,15.065002 L33.8636338,5.0280579 C33.031014,4.22181378 31.7191142,4.23657731 30.9042766,5.05039756 Z" id="Combined-Shape"></path>
+                            </g>
+                        </g>
+                        <polyline id="Line" stroke="#000000" stroke-width="6" stroke-linecap="round" stroke-linejoin="round" points="12 9.21367521 0 18.2136752 12 27.2136752"></polyline>
+                        <polyline id="Line" stroke="#000000" stroke-width="6" stroke-linecap="round" stroke-linejoin="round" transform="translate(80.000000, 39.213675) scale(-1, 1) translate(-80.000000, -39.213675) " points="86 30.2136752 74 39.2136752 86 48.2136752"></polyline>
+                    </g>
+                </g>
+            </g>
+        </g>
+    </g>
+</svg>
 };
 export default icon;
+
