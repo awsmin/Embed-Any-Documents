420534152269ea98acdf3b5fc581c3760328040f
Sruthi H S <sruthi@awsm.in>
2026-05-07 11:31:34 +0530

    FIXED:https://awsm.atlassian.net/browse/EAD-4

--- awsm-embed.php
+++ awsm-embed.php
@@ -456,6 +456,9 @@ class Awsm_embed {
 				}
 			}
 			$url = esc_url( $shortcode_atts['url'], array( 'http', 'https' ) );
+			if ( empty( $url ) ) {
+				return esc_html__( 'No Url Found', 'embed-any-document' );
+			}
 			if ( $show ) {
 				$filedata = wp_safe_remote_head( $shortcode_atts['url'] );
 				$filesize = 0;
